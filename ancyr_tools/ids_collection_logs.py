2cdcfbafeb8c1f34fba43b36675567d5e6080474
Adam <ams.schafer@gmail.com>
2023-07-25 00:34:49 -0400

    Converting to name (with some names missing)

--- ancyr_tools/ids_collection_logs.py
+++ ancyr_tools/ids_collection_logs.py
@@ -4,7 +4,7 @@ import csv
 
 class IdsCollectionSample:
 
-    def __init__(self, offset: str, cycles: int, icache_misses: int, dcache_misses: int, seconds: float):
+    def __init__(self, offset: int, cycles: int, icache_misses: int, dcache_misses: int, seconds: float):
         self.offset = offset
         self.cycles = cycles
         self.icache_misses = icache_misses
@@ -20,7 +20,24 @@ class IdsCollectionSample:
                 # print(f"row: {row}")
                 retval.append(
                     IdsCollectionSample(
-                        row[0], int(row[1]), int(row[2]), int(row[3]), float(row[4])
+                        int(row[0], 0), int(row[1]), int(row[2]), int(row[3]), float(row[4])
                     )
                 )
         return retval
+
+    @staticmethod
+    def get_function_names(ids_samples: ["IdsCollectionSample"], symbols_by_offset: dict) -> [str]:
+        """
+        Get a list of the function names for the provided ids_samples array and the samples dictionary
+        :param ids_samples:
+        :param symbols_by_offset:
+        :return:
+        """
+        retval = []
+        for sample in ids_samples:
+            if sample.offset not in symbols_by_offset:
+                retval.append(hex(sample.offset))
+            else:
+                retval.append(symbols_by_offset[sample.offset]["name"])
+        return retval
+
--- tests/test_ids_collection_logs.py
+++ tests/test_ids_collection_logs.py
@@ -1,8 +1,10 @@
 from unittest import TestCase
 from pathlib import Path
 from ancyr_tools.ids_collection_logs import IdsCollectionSample
+from ancyr_tools.symbol_parser import parse_symbol_file
 
 SAMPLE_LOG_FILES_DIR = Path(__file__).parent.joinpath("samples", "ids_collection_logs")
+SAMPLE_SYMBOL_FILES_DIR = Path(__file__).parent.joinpath("samples", "symbol_files")
 
 # CSV file is formatted as:
 # offset, hw cycles, i_cache misses, d_cache mises, real time
@@ -14,8 +16,17 @@ class TestLoadCollectionLogsFile(TestCase):
         self.assertEqual(5000, len(ids_samples))
         # Verify that the first sample is valid
         sample = ids_samples[0]
-        self.assertEqual("0x12470", sample.offset)
+        self.assertEqual(74864, sample.offset)
         self.assertEqual(24371, sample.cycles)
         self.assertEqual(314, sample.icache_misses)
         self.assertEqual(140, sample.dcache_misses)
         self.assertEqual(18688.337555851, sample.seconds)
+
+    def test_get_sample_function_names(self):
+        sample_ids_log_file = SAMPLE_LOG_FILES_DIR.joinpath("ids_func_0x10f70.csv")
+        sample_symbol_file = SAMPLE_SYMBOL_FILES_DIR.joinpath("sym.txt")
+        symbols_by_offset, _ = parse_symbol_file(sample_symbol_file)
+
+        ids_samples = IdsCollectionSample.load_ids_collection_log(sample_ids_log_file)
+        function_names = IdsCollectionSample.get_function_names(ids_samples, symbols_by_offset)
+        self.assertEqual(5000, len(function_names))
